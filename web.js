d42e11ddaefa629918d528c4ffdfe7b66a427f01
jefftash <jefftash@comcast.net>
2013-07-14 18:08:29 +0000

    Modified web.js to read from index.html

--- web.js
+++ web.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var buffer = new Buffer(fs.readFileSync("index.html"));
+
 app.get('/', function(request, response) {
-  var fs = require('fs');
-  var buffer = new Buffer(fs.readFileSync("index.html", "utf-8"));
   response.send(buffer.toString());
 });
 
