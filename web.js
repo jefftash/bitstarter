179828ef0d72ab63c2ea0379bb505c1062d260b5
Jeff Tash <jefftash@comcast.net>
2013-07-15 18:39:19 +0000

    removed erroneous garbled text

--- web.js
+++ web.js
@@ -4,15 +4,13 @@ var app = express.createServer(express.logger());
 
 var fs = require('fs');
 
-<<<<<<< HEAD
 var buffer = new Buffer(fs.readFileSync("index.html"));
-=======
-var buffer = new Buffer(fs.readFileSync('index.html'));
->>>>>>> 7b8633d0df8664ced6c6871fa08de17779c27e66
+
 
 app.get('/', function(request, response) {
   response.send(buffer.toString());
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
